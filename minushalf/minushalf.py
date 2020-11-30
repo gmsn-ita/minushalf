62b3aecb5b133931778c2af77cce352e8b06c27a
Henrique <hentt30@gmail.com>
2020-11-30 08:23:56 -0300

    feat: Adding modified vbm-character command

--- minushalf/minushalf.py
+++ minushalf/minushalf.py
@@ -2,7 +2,8 @@
 Definition of the minushalf CLI
 """
 import click
-from .commands import (run_atomic, band_character, occupation, correct_potfile)
+from .commands import (run_atomic, band_character, occupation, correct_potfile,
+                       vbm_character)
 
 
 @click.group()
@@ -16,3 +17,4 @@ minushalf.add_command(run_atomic)
 minushalf.add_command(band_character)
 minushalf.add_command(occupation)
 minushalf.add_command(correct_potfile)
+minushalf.add_command(vbm_character)
