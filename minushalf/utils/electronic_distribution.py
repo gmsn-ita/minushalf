4e93a563ac02638f99678d96ea0c2619375d5f78
Henrique <hentt30@gmail.com>
2020-12-05 13:48:39 -0300

    tests: Adding tests to eletrocnic distribution

--- minushalf/utils/electronic_distribution.py
+++ minushalf/utils/electronic_distribution.py
@@ -6,7 +6,7 @@ from enum import Enum, unique
 
 
 @unique
-class EletronicDistribution(Enum):
+class ElectronicDistribution(Enum):
     """
     Enum type for orbital type. Indices are basically the azimuthal quantum
     number, l.
