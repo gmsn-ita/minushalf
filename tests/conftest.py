753151755e3a2a69ec12b6c968d69af0f7d24000
Henrique <hentt30@gmail.com>
2020-12-10 19:36:42 -0300

    [tests]: Conftest.py adds automatically the package in the PYTHONPATH

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,12 @@
 File for test configuration
 """
 import os
+import sys
 import pytest
+
+path_to_script = os.path.realpath(__file__)
+path_to_package = os.path.join(path_to_script, "../minushalf")
+sys.path.append(path_to_package)
 
 
 @pytest.fixture
@@ -13,11 +18,11 @@ def file_path():
     Args:
         filename (str): name of the file.
     """
-
     def _foo(filename: str):
 
         path_to_script = os.path.realpath(__file__)
         current_directory = os.path.split(path_to_script)[0]
-        return os.path.join(current_directory, "fixtures/{}".format(filename)).__str__()
+        return os.path.join(current_directory,
+                            "fixtures/{}".format(filename)).__str__()
 
     return _foo
