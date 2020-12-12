049d2dd89f85772b256e167148907fe06aa04350
Henrique <hentt30@gmail.com>
2020-12-12 19:54:10 -0300

    [tests]: Adding tests to atomic potential class

---
+++ tests/unit/utils/test_constants.py
@@ -0,0 +1,37 @@
+"""
+Test constants
+"""
+import numpy as np
+from minushalf.utils import Constants
+
+
+def test_pi():
+    """
+    Test pi in constants module
+    """
+    constants = Constants()
+    assert np.isclose(constants.pi_constant, 3.1415926548)
+
+
+def test_trimming_exponent():
+    """
+    Test trimming exponent in constants module
+    """
+    constants = Constants()
+    assert np.isclose(constants.trimming_exponent, 8)
+
+
+def test_bohr_radius():
+    """
+    Test bohr radius in constants module
+    """
+    constants = Constants()
+    assert np.isclose(constants.bohr_radius, 0.529177068)
+
+
+def test_rydberg():
+    """
+    Test Rydberg constant in constants module
+    """
+    constants = Constants()
+    assert np.isclose(constants.rydberg, 13.6058038)
---
+++ tests/unit/utils/test_correct_potential_fourier_transform.py
@@ -0,0 +1,176 @@
+"""
+Test correct potential fourier transform
+"""
+import pytest
+import numpy as np
+from minushalf.utils import correct_potential_fourier_transform
+
+
+def test_correct_potential_fourier_transform_first():
+    """
+    Test correct_potential_fourier_transform with the
+    following parameters:
+
+    coefficient: 0
+    k: 0
+    rays: np.arange(0,5,0.1)
+    occupation_potential: np.arange(20,25,0.1)
+    cut: 3.0
+
+    """
+    res = correct_potential_fourier_transform(0, 0, np.arange(0, 5, 0.1),
+                                              np.arange(20, 25, 0.1), 3.0)
+    assert np.isclose(res, 92.23450000000014)
+
+
+def test_correct_potential_fourier_transform_second():
+    """
+    Test correct_potential_fourier_transform with the
+    following parameters:
+
+    coefficient: 1
+    k: 0
+    rays: np.arange(0,5,0.1)
+    occupation_potential: np.arange(20,25,0.1)
+    cut: 3.0
+
+    """
+    res = correct_potential_fourier_transform(1, 0, np.arange(0, 5, 0.1),
+                                              np.arange(20, 25, 0.1), 3.0)
+    assert np.isclose(res, 93.23450000000014)
+
+
+def test_correct_potential_fourier_transform_third():
+    """
+    Test correct_potential_fourier_transform with the
+    following parameters:
+
+    coefficient: 1
+    k: 2e-1
+    rays: np.arange(0,5,0.1)
+    occupation_potential: np.arange(20,25,0.1)
+    cut: 3.0
+
+    """
+    res = correct_potential_fourier_transform(1, 2e-1, np.arange(0, 5, 0.1),
+                                              np.arange(20, 25, 0.1), 3.0)
+    assert np.isclose(res, 92.49911868134099)
+
+
+def test_correct_potential_fourier_transform_fourth():
+    """
+    Test correct_potential_fourier_transform with the
+    following parameters:
+
+    coefficient: 4
+    k: 3
+    rays: np.arange(12,19,0.01)
+    occupation_potential: np.arange(70,79,0.01)
+    cut: 13.6
+
+    """
+    res = correct_potential_fourier_transform(4, 3, np.arange(12, 19, 0.01),
+                                              np.arange(70, 79, 0.01), 13.6)
+    assert np.isclose(res, 110.19498579254733)
+
+
+def test_correct_potential_fourier_transform_fifth():
+    """
+    Test correct_potential_fourier_transform with the
+    following parameters:
+
+    coefficient: 0
+    k: 3
+    rays: np.arange(12,19,0.01)
+    occupation_potential: np.arange(70,79,0.01)
+    cut: 13.6
+
+    """
+    res = correct_potential_fourier_transform(0, 3, np.arange(12, 19, 0.01),
+                                              np.arange(70, 79, 0.01), 13.6)
+    assert np.isclose(res, 106.19498579254733)
+
+
+@pytest.mark.xfail
+def test_correct_potential_fourier_transform_sixth():
+    """
+    Test correct_potential_fourier_transform with the
+    following parameters:
+
+    coefficient: 0
+    k: 3
+    rays: np.arange(12,19,0.01)
+    occupation_potential: np.arange(70,79,0.01)
+    cut: 11.6
+
+    """
+    correct_potential_fourier_transform(0, 3, np.arange(12, 19, 0.01),
+                                        np.arange(70, 79, 0.01), 11.6)
+
+
+def test_correct_potential_fourier_transform_seventh():
+    """
+    Test correct_potential_fourier_transform with the
+    following parameters:
+
+    coefficient: 0
+    k: 67
+    rays: np.arange(0,12,0.01)
+    occupation_potential: np.arange(70,79,0.01)
+    cut: 1.23
+
+    """
+    res = correct_potential_fourier_transform(0, 67, np.arange(0, 12, 0.01),
+                                              np.arange(70, 79, 0.01), 1.23)
+    assert np.isclose(res, 0.013212154569150683)
+
+
+def test_correct_potential_fourier_transform_eighth():
+    """
+    Test correct_potential_fourier_transform with the
+    following parameters:
+
+    coefficient: 0
+    k: 67
+    rays: np.arange(0,12,0.1)
+    occupation_potential: np.arange(70,79,0.1)
+    cut: 1.23
+
+    """
+    res = correct_potential_fourier_transform(0, 67, np.arange(0, 12, 0.1),
+                                              np.arange(70, 79, 0.1), 1.23)
+    assert np.isclose(res, -0.017530596213630182)
+
+
+def test_correct_potential_fourier_transform_nineth():
+    """
+    Test correct_potential_fourier_transform with the
+    following parameters:
+
+    coefficient: 0
+    k: 67
+    rays: np.arange(0,12,0.2)
+    occupation_potential: np.arange(70,79,0.2)
+    cut: 1.23
+
+    """
+    res = correct_potential_fourier_transform(0, 67, np.arange(0, 12, 0.2),
+                                              np.arange(70, 79, 0.2), 1.23)
+    assert np.isclose(res, 0.3972120670340923)
+
+
+def test_correct_potential_fourier_transform_tenth():
+    """
+    Test correct_potential_fourier_transform with the
+    following parameters:
+
+    coefficient: 0
+    k: 67
+    rays: np.arange(0,12,0.4)
+    occupation_potential: np.arange(70,79,0.4)
+    cut: 1.23
+
+    """
+    res = correct_potential_fourier_transform(0, 67, np.arange(0, 12, 0.4),
+                                              np.arange(70, 79, 0.4), 1.23)
+    assert np.isclose(res, 0.32399512659710605)
