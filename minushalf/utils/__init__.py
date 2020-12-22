c13f729dea08b65d8d466d6bdb4cdf67d47e7fab
Henrique <hentt30@gmail.com>
2020-12-22 14:21:35 -0300

    [feat]: Adding ternary search to utils [x] Write algorithm to ternary search for find maximum in unimodal functions [x] Write five tests to this function

--- minushalf/utils/__init__.py
+++ minushalf/utils/__init__.py
@@ -11,3 +11,4 @@ from .constants import Constants
 from .trimming_function import trimming_function
 from .correct_potential_fourier_transform import correct_potential_fourier_transform
 from .projection_to_df import projection_to_df
+from .ternary_search import ternary_search
