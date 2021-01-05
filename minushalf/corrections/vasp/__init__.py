a35b09a4a44712d6db05af6ef07f4fd00c5768ff
Henrique <hentt30@gmail.com>
2021-01-05 17:50:00 -0300

    [feat] Adding algorithms for simple and fractionary conduction algorithms

--- minushalf/corrections/vasp/__init__.py
+++ minushalf/corrections/vasp/__init__.py
@@ -2,3 +2,6 @@
 Init for vasp correction module
 """
 from .valence import VaspValenceCorrection
+from .valence_fractionary import VaspValenceFractionaryCorrection
+from .conduction_fractionary import VaspConductionFractionaryCorrection
+from .conduction import VaspConductionCorrection
