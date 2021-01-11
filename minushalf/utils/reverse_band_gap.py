450a72ef19e12db6dd52829b39635ab1b835852a
Henrique <hentt30@gmail.com>
2021-01-11 15:16:49 -0300

    [feat]: Checking if potential folder really exists and throw a error if not

--- minushalf/utils/reverse_band_gap.py
+++ minushalf/utils/reverse_band_gap.py
@@ -98,8 +98,8 @@ def find_reverse_band_gap(cuts: list, *args: tuple) -> float:
         """
     extra_args = args[0]
     cut = cuts[0]
-    if cut <= extra_args["atom_potential"].vtotal.radius[0]:
-        return (-1) * math.inf
+    if cut <= extra_args["atom_potential"].vtotal.radius[1]:
+        return math.inf
     runner = extra_args["runner"]
     software_factory = extra_args["software_factory"]
 
