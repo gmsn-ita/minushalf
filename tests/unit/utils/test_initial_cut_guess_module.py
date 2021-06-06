7fcaf9c7f45406f9ad971f4e0a954eff8a3f9f1a
Henrique <hentt30@gmail.com>
2021-06-06 16:22:27 -0300

    [tests]: Changind wrong unit tests

--- tests/unit/utils/test_initial_cut_guess_module.py
+++ tests/unit/utils/test_initial_cut_guess_module.py
@@ -7,12 +7,12 @@ from minushalf.utils import CutInitialGuess
 
 def test_3d_method():
     """
-    Test the method of guess for 2d compunds
+    Test the method of guess for 3d compunds
     """
     cut_guess = CutInitialGuess()
     guess_1 = cut_guess.guess(0, "3d")
     assert np.isclose(guess_1, 0.15)
     guess_1 = cut_guess.guess(1, "3d")
-    assert np.isclose(guess_1, 0.99)
+    assert np.isclose(guess_1, 0.84 * 1.88973 + 0.15)
     guess_1 = cut_guess.guess(2, "3d")
-    assert np.isclose(guess_1, 1.83)
+    assert np.isclose(guess_1, 0.84 * 1.88973 * 2 + 0.15)
