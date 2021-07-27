ed962b3913e0c98d6ed762dd0dcd1c033b5d4d5b
Henrique <hentt30@gmail.com>
2021-07-27 13:21:35 -0300

    [tests]: Adding tests to Correction class

---
+++ tests/unit/io/test_correction.py
@@ -0,0 +1,89 @@
+"""
+Test correction input class
+"""
+import numpy as np
+from minushalf.io import Correction, correction
+
+
+def test_default_parameters():
+    """
+    Test the default parameters 
+    """
+    correction = Correction()
+
+    assert correction.correction_code == "v"
+    assert correction.potfiles_folder == "minushalf_potfiles"
+    assert np.isclose(correction.amplitude, 1.0)
+    assert correction.valence_cut_guess == None
+    assert correction.conduction_cut_guess == None
+    assert np.isclose(correction.tolerance, 0.01)
+    assert correction.fractional_valence_treshold == 10
+    assert correction.fractional_conduction_treshold == 9
+    assert correction.overwrite_cbm == []
+    assert correction.overwrite_vbm == []
+    assert correction.inplace == False
+
+
+def test_override_parameters():
+    """
+    Test the class with other parameters
+    """
+    params = {
+        "correction_code": "vf",
+        "potfiles_folder": "potfiles",
+        "amplitude": 2.0,
+        "valence_cut_guess": 3.34,
+        "conduction_cut_guess": 3.34,
+        "tolerance": 0.1,
+        "fractional_valence_treshold": 12,
+        "fractional_conduction_treshold": 14,
+        "overwrite_vbm": [1, 2],
+        "overwrite_cbm": [1, 3],
+        "inplace": True
+    }
+    correction = Correction(**params)
+    assert correction.correction_code == "vf"
+    assert correction.potfiles_folder == "potfiles"
+    assert np.isclose(correction.amplitude, 2.0)
+    assert correction.valence_cut_guess == 3.34
+    assert correction.conduction_cut_guess == 3.34
+    assert np.isclose(correction.tolerance, 0.1)
+    assert correction.fractional_valence_treshold == 12
+    assert correction.fractional_conduction_treshold == 14
+    assert correction.overwrite_cbm == [1, 3]
+    assert correction.overwrite_vbm == [1, 2]
+    assert correction.inplace == True
+
+
+def test_to_list():
+    """
+    Test method to_list
+    """
+    params_list = [
+        "v", "minushalf_potfiles", 1.0, None, None, 0.01, 10, 9, [], [], False
+    ]
+    correction = Correction()
+
+    assert correction.to_list() == params_list
+
+
+def test_to_dict():
+    """
+    Test method to_list
+    """
+    params = {
+        "correction_code": "vf",
+        "potfiles_folder": "potfiles",
+        "amplitude": 2.0,
+        "valence_cut_guess": 3.34,
+        "conduction_cut_guess": 3.34,
+        "tolerance": 0.1,
+        "fractional_valence_treshold": 12,
+        "fractional_conduction_treshold": 14,
+        "overwrite_vbm": [1, 2],
+        "overwrite_cbm": [1, 3],
+        "inplace": True
+    }
+    correction = Correction(**params)
+
+    assert correction.to_dict() == params
