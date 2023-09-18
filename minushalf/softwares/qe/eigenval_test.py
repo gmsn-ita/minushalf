f587a431f338ce1737de4d6f92c2f61e00990939
ivanguilhon <ivanguilhonn@gmail.com>
2023-09-18 11:37:15 -0300

    Vamos implementar o minushalf no quantum espresso

---
+++ minushalf/softwares/qe/eigenval_test.py
@@ -0,0 +1,91 @@
+"""
+Test Eigenval module
+The functions in this file obey the following rules:
+test_(what the function is meant to do)_(chemical compound)
+"""
+from minushalf.softwares.vasp.eigenval import Eigenvalues
+
+
+def test_parse_eigenvalues_gan_3d(file_path):
+    """
+    Test if the eigenvalues module
+    collect all the eigenvalues.
+    """
+    filename = file_path("/gan-3d/EIGENVAL")
+    eigenval = Eigenvalues(filename)
+
+    number_of_bands = 16
+    number_of_kpoints = 10
+    for _, band in eigenval.eigenvalues.items():
+
+        assert len(band) == number_of_bands
+
+    assert len(eigenval.eigenvalues) == number_of_kpoints
+
+
+def test_parse_eigenvalues_bn_2d(file_path):
+    """
+    Test if the eigenvalues module
+    collect all the eigenvalues.
+    """
+    filename = file_path("/bn-2d/EIGENVAL")
+    eigenval = Eigenvalues(filename)
+
+    number_of_bands = 8
+    number_of_kpoints = 24
+    for _, band in eigenval.eigenvalues.items():
+
+        assert len(band) == number_of_bands
+
+    assert len(eigenval.eigenvalues) == number_of_kpoints
+
+
+def test_parse_eigenvalues_sic_2d(file_path):
+    """
+    Test if the eigenvalues module
+    collect all the eigenvalues.
+    """
+    filename = file_path("/sic-2d/EIGENVAL")
+    eigenval = Eigenvalues(filename)
+
+    number_of_bands = 16
+    number_of_kpoints = 27
+    for _, band in eigenval.eigenvalues.items():
+
+        assert len(band) == number_of_bands
+
+    assert len(eigenval.eigenvalues) == number_of_kpoints
+
+
+def test_parse_eigenvalues_gec_2d(file_path):
+    """
+    Test if the eigenvalues module
+    collect all the eigenvalues.
+    """
+    filename = file_path("/gec-2d/EIGENVAL")
+    eigenval = Eigenvalues(filename)
+
+    number_of_bands = 16
+    number_of_kpoints = 12
+    for _, band in eigenval.eigenvalues.items():
+
+        assert len(band) == number_of_bands
+
+    assert len(eigenval.eigenvalues) == number_of_kpoints
+
+
+def test_parse_eigenvalues_aln_2d(file_path):
+    """
+    Test if the eigenvalues module
+    collect all the eigenvalues.
+    """
+    filename = file_path("/aln-2d/EIGENVAL")
+    eigenval = Eigenvalues(filename)
+
+    number_of_bands = 8
+    number_of_kpoints = 16
+    for _, band in eigenval.eigenvalues.items():
+
+        assert len(band) == number_of_bands
+
+    assert len(eigenval.eigenvalues) == number_of_kpoints
---
+++ minushalf/softwares/qe/vasp_runner_test.py
@@ -0,0 +1,41 @@
+"""
+Test vasp runner class
+"""
+import subprocess
+from unittest import mock
+from minushalf.softwares.vasp.runner import VaspRunner
+
+
+def test_with_number_of_cores_1():
+    """
+    Test vasprunner class with 1 core
+    and path to vasp with default
+    """
+    with mock.patch("subprocess.run"):
+        command = ["mpirun", "-np", "1", "vasp"]
+        runner = VaspRunner(command)
+        runner.run()
+        subprocess.run.assert_called_once_with(command, check=True, cwd=".")
+
+
+def test_with_number_of_cores_4():
+    """
+    Test vasprunner class with 4 core
+    and path to vasp with '../vasp'
+    """
+    with mock.patch("subprocess.run"):
+        command = ["mpirun", "-np", "4", "../vasp"]
+        runner = VaspRunner(command)
+        runner.run()
+        subprocess.run.assert_called_once_with(command, check=True, cwd=".")
+
+
+def test_with_different_path():
+    """
+    Test vasprunner class with a different execution path
+    """
+    with mock.patch("subprocess.run"):
+        command = ["mpirun", "-np", "4", "../vasp"]
+        runner = VaspRunner(command)
+        runner.run(cwd="./ss")
+        subprocess.run.assert_called_once_with(command, check=True, cwd="./ss")
