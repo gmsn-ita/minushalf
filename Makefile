c80f69163889c31575c7a24d16a28bc1dec116af
BACStrange <augusto_cavalcanti07@hotmail.com>
2026-05-25 03:37:54 -0300

    Modern Python Update

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 virtual_env:
-	poetry env use python3.6
+	poetry env use python3.14
 	poetry shell
 
-.PHONY: usepy36
+.PHONY: usepy314
--- setup.py
+++ setup.py
@@ -1,26 +1,47 @@
 """
 Configuration setup for python project
 """
+import os
 from os import path
+import sys
 import setuptools
-from numpy.distutils.core import Extension, setup
+from setuptools import Extension
+from setuptools.command.build_ext import build_ext
+
 
 this_directory = path.abspath(path.dirname(__file__))
+ATOMIC_DIR = path.join(this_directory, "minushalf", "atomic_program")
+
 with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-atomic_program = Extension(name="minushalf.atomic_program",
-                           sources=[
-                               "minushalf/atomic_program/atm_cGuima3.f",
-                               "minushalf/atomic_program/atm_cGuima3.pyf"
-                           ],
-                           extra_compile_args=['-static'])
+class f2py_Extension(Extension):
+    def __init__(self, name, sourcedir):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = path.abspath(sourcedir)
 
-setup(
+
+class f2py_Build(build_ext):
+    def run(self):
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        print("Building this shit")
+        os.system(
+            f"cd {ext.sourcedir} && {sys.executable} -m numpy.f2py"
+            f" -c atm_cGuima3.f atm_cGuima3.pyf"
+            f" --include-paths={ext.sourcedir} -m atomic_program"
+        )
+
+setuptools.setup(
     name="minushalf",
     version="1.8",
     packages=setuptools.find_packages(),
     include_package_data=True,
+    package_data={
+        "minushalf.atomic_program": ["*.so"],   
+    },
     author="Henrique Fernandes",
     author_email="dftminushalf@gmail.com",
     description=
@@ -28,20 +49,21 @@ setup(
     long_description=long_description,
     license="GPL",
     install_requires=[
-        "pandas==1.1.5",
-        "fortranformat==0.2.5",
-        "Click==7.1.2",
-        "pyfiglet==0.8.post1",
-        "loguru==0.5.3",
-        "tabulate==0.8.10",
-        "numpy==1.19.5",
-        "pyyaml",
-        "scipy==1.5.4",
-        "aenum==3.0.0",
+        "pandas==3.0.3",
+        "fortranformat==2.0.3",
+        "click==8.1.0",
+        "pyfiglet==1.0.4",
+        "loguru==0.7.3",
+        "tabulate==0.9.0",
+        "numpy==2.4.6",
+        "pyyaml==6.0.1",
+        "scipy==1.17.1",
+        "aenum==3.1.17",
     ],
     entry_points="""
         [console_scripts]
         minushalf=minushalf.minushalf:minushalf
     """,
-    ext_modules=[atomic_program],
+    ext_modules=[f2py_Extension('minushalf.atomic_program.atomic_program', ATOMIC_DIR)],
+    cmdclass=dict(build_ext=f2py_Build),
 )
